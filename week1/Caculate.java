dcb9ca69f1c85c48e9237dbaac6b578a7750c08b
Cashsu <nghia.hnkt@gmail.com>
2022-10-27 23:13:56 +0700

    init code

--- week1/Caculate.java
+++ week1/Caculate.java
@@ -2,11 +2,7 @@ package week1;
 
 import javax.swing.JOptionPane;
 
-public class ShowToNumbers {
-    public static void print() {
-        System.out.println("hello");
-    }
-
+public class Caculate {
     public static void main(String[] args) {
         String strNum1, strNum2;
         String strNotification = "You've just entered: ";
@@ -14,15 +10,24 @@ public class ShowToNumbers {
         strNum1 = JOptionPane.showInputDialog(null, "Please input the first number: ",
                 "Input your first number",
                 JOptionPane.INFORMATION_MESSAGE);
-        strNotification += strNum1 + " and ";
 
+        double num1 = Double.parseDouble(strNum1);
         strNum2 = JOptionPane.showInputDialog(null, "Please input the second number: ",
                 "Input your first number",
                 JOptionPane.INFORMATION_MESSAGE);
-        strNotification += strNum2;
+        double num2 = Double.parseDouble(strNum2);
+
+        double sum = num1 + num2;
+        double difference = num1 - num2;
+        double product = num1 * num2;
+        double quotient = num1 / num2;
+
+        strNotification = "Thank you \n" + "Sum is: " + sum + "\n" +
+                "Difference is: " + difference + "\n" +
+                "Product is: " + product + "\n" +
+                "Quotient is: " + quotient + "\n";
 
         JOptionPane.showMessageDialog(null, strNotification, "Show two numbers", JOptionPane.ERROR_MESSAGE);
         System.exit(0);
     }
-
 }
